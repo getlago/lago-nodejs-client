0ec6fea159376de557c44ee5873b2fe432712cf7
Romain Sempé <romain@getlago.com>
2023-01-16 11:26:24 +0100

    feat: Add invoice grace period to customer

--- test/customer.test.js
+++ test/customer.test.js
@@ -5,25 +5,30 @@ import Customer from '../lib/models/customer.js';
 import CustomerBillingConfiguration from '../lib/models/customer_billing_configuration.js';
 
 let client = new Client('api_key')
-let customer = new Customer(
-    "5eb02857-a71e-4ea2-bcf9-57d8885990ba", // externalId
-    "Gavin Belson", // name
-    null, // addressLine1
-    null, // addressLine2
-    null, // city
-    null, // country
-    null, // currency
-    null, // email
-    null, // legalName
-    null, // legalNumber
-    null, // logoUrl
-    null, // phone
-    null, // state
-    null, // timezone
-    null, // url
-    null, // zipcode
-    new CustomerBillingConfiguration("stripe", "cus_12345"), // billingConfiguration
-)
+
+let customer = new Customer({
+    externalId: "5eb02857-a71e-4ea2-bcf9-57d8885990ba",
+    name: "Gavin Belson",
+    addressLine1: null,
+    addressLine2: null,
+    city: null,
+    country: null,
+    currency: null,
+    email: null,
+    legalName: null,
+    legalNumber: null,
+    logoUrl: null,
+    phone: null,
+    state: null,
+    timezone: null,
+    url: null,
+    zipcode: null,
+    customerBillingConfiguration: new CustomerBillingConfiguration({
+        invoiceGracePeriod: 3,
+        paymentProvider: "stripe",
+        providerCustomerId: "cus_12345",
+    })
+})
 
 describe('Successfully sent customer responds with 2xx', () => {
     before(() => {
